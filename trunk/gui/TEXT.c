0ef629959de457ea9118d1976658836db08d4bdb
tehpola@yahoo.com <tehpola@yahoo.com@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-10-14 04:11:29 +0000

    DEBUG and GUI systems (may not be working properly)

---
+++ trunk/gui/TEXT.c
@@ -0,0 +1,47 @@
+/* TEXT.c - Helper functions for reformatting text
+   by Mike Slegeir for Mupen64-GC
+ */
+
+#include "TEXT.h"
+
+char TEXT_split_lines[TEXT_MAX_SPLIT][TEXT_WIDTH];
+
+int TEXT_split(char* string){
+	int current_line = 0, i = 0;
+	while( *string != 0 ){
+		if( *string == '\n' ){
+			if(++current_line >= TEXT_MAX_SPLIT) return current_line;
+			i = 0;
+		} else TEXT_split[current_line][i++] = *string;
+		
+		++string;
+	}
+}
+
+void TEXT_expand(char* string){
+	// Although it'd be nice to get this in one sweep, it'd probably not be a good idea
+	
+	// First determine if every tab is expandable without overflow
+	int i, spaces = 0;
+	for(i=0; string[i] != 0; ++i)
+		if(string[i] == '\t') spaces += TEXT_SPACE_PER_TAB - 1;
+	
+	if( TEXT_WIDTH - i > spaces ){ // We have the space to expand
+		while(i >= 0){
+			if(string[i] == '\t'){ // Expand this tab
+				int j;
+				for(j=0; j<TEXT_SPACE_PER_TAB; ++j)
+					string[i+spaces-j] = ' ';
+				spaces -= TEXT_SPACE_PER_TAB - 1;
+				
+			} else // Otherwise just move this character forward
+				string[i+spaces] = string[i];
+			
+			--i;
+		}
+		
+	} else { // Just translate every tab into a single space
+		for(i=0; string[i] != 0; ++i)
+			if(string[i] == '\t') string[i] = ' ';
+	}
+}
---
+++ trunk/gui/TEXT.h
@@ -0,0 +1,27 @@
+/* TEXT.h - Helper functions for reformatting text
+   by Mike Slegeir for Mupen64-GC
+ */
+
+#ifndef TEXT_H
+#define TEXT_H
+
+// Maximum number of lines that can be in one split
+#define TEXT_MAX_SPLIT 5
+#define TEXT_WIDTH     64
+
+extern char TEXT_split_lines[TEXT_MAX_SPLIT][TEXT_WIDTH];
+
+// Splits the text on newlines
+// string:  the string to split
+// RETURNS: number of lines split
+//          Fills out TEXT_split[0 thru return value]
+int TEXT_split(char* string);
+
+#define TEXT_SPACE_PER_TAB 3
+// Expands tabs into TEXT_SPACE_PER_TAB spaces
+// string:      string to expand
+// Assumes buffer size is TEXT_WIDTH, won't overflow that size
+void TEXT_expand(char* string);
+
+#endif
+
