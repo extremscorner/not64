c8bf4b44f834bbc489da0e0104744b3744bf49a7
tehpola@yahoo.com <tehpola@yahoo.com@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-10-17 15:08:28 +0000

    TLB-Cache-hash added (TLB_LUT using a hash map in MRAM)

---
+++ gc_memory/TLB-Cache-hash.c
@@ -0,0 +1,108 @@
+/* TLB-Cache-hash.c - This is how the TLB LUT should be accessed, using a hash map
+   by Mike Slegeir for Mupen64-GC
+ */
+
+#include <stdlib.h>
+#include "TLB-Cache.h"
+
+// Num Slots must be a power of 2!
+#define TLB_NUM_SLOTS 64
+
+typedef struct node {
+	unsigned int value;
+	unsigned int page;
+	struct node* next;
+} TLB_hash_node;
+
+static TLB_hash_node* TLB_LUT_r[TLB_NUM_SLOTS];
+static TLB_hash_node* TLB_LUT_w[TLB_NUM_SLOTS];
+
+static unsigned int TLB_hash_shift;
+
+void TLBCache_init(void){
+	unsigned int temp = TLB_NUM_SLOTS;
+	while(temp){
+		temp >>= 1;
+		++TLB_hash_shift;
+	}
+}
+
+void TLBCache_deinit(void){
+	int i;
+	TLB_hash_node* node, * next;
+	for(i=0; i<TLB_NUM_SLOTS; ++i){
+		// Clear r
+		for(node = TLB_LUT_r[i]; node != NULL; node = next){
+			next = node->next;
+			free(node);
+		}
+		TLB_LUT_r[i] = NULL;
+		
+		// Clear w
+		for(node = TLB_LUT_w[i]; node != NULL; node = next){
+			next = node->next;
+			free(node);
+		}
+		TLB_LUT_w[i] = NULL;
+	}
+}
+
+static unsigned int inline TLB_hash(unsigned int page){
+	return page >> TLB_hash_shift;
+}
+
+unsigned int inline TLBCache_get_r(unsigned int page){
+	TLB_hash_node* node = TLB_LUT_r[ hash(page) ];
+	
+	for(; node != NULL; node = node->next)
+		if(node->page == page) return node->value;
+	
+	return 0;
+}
+
+unsigned int inline TLBCache_get_w(unsigned int page){
+	TLB_hash_node* node = TLB_LUT_w[ hash(page) ];
+	
+	for(; node != NULL; node = node->next)
+		if(node->page == page) return node->value;
+	
+	return 0;
+}
+
+void inline TLBCache_set_r(unsigned int page, unsigned int val){
+	TLB_hash_node* next = TLB_LUT_r[ hash(page) ];
+	
+	TLB_hash_node* node = malloc( sizeof(TLB_hash_node) );
+	node->page  = page;
+	node->value = val;
+	node->next  = next;
+	
+	// Remove any old values for this page from the linked list
+	for(; node != NULL; node = node->next)
+		if(node->next != NULL && node->next->page == page){
+			TLB_hash_node* old_node = node->next;
+			node->next = old_node->next;
+			free( old_node );
+			break;
+		}
+}
+
+void inline TLBCache_set_w(unsigned int page, unsigned int val){
+	TLB_hash_node* next = TLB_LUT_w[ hash(page) ];
+	
+	TLB_hash_node* node = malloc( sizeof(TLB_hash_node) );
+	node->page  = page;
+	node->value = val;
+	node->next  = next;
+	
+	// Remove any old values for this page from the linked list
+	for(; node != NULL; node = node->next)
+		if(node->next != NULL && node->next->page == page){
+			TLB_hash_node* old_node = node->next;
+			node->next = old_node->next;
+			free( old_node );
+			break;
+		}
+}
+
+
