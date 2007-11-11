949e13847f84464be0f57c0cf805ef8aa516ddd4
tehpola <tehpola@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-11-11 03:22:28 +0000

    Rewrote DEBUG, it should (crosses fingers) actually work once a backend is written for it.

--- trunk/gui/DEBUG.h
+++ trunk/gui/DEBUG.h
@@ -5,10 +5,8 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-// The max number of strings that will be held onto
-#define DEBUG_NUM_STRINGS 25
 // Amount of time each string will be held onto
-#define DEBUG_STRING_LIFE 2.0f
+#define DEBUG_STRING_LIFE 3000
 // Dimensions of array returned by get_text
 #define DEBUG_TEXT_WIDTH  64
 #define DEBUG_TEXT_HEIGHT 20
@@ -17,7 +15,7 @@
 void DEBUG_print(char* string);
 
 // Should be called before get_text. Ages the strings, and remove old ones
-void DEBUG_update(float dt);
+void DEBUG_update(void);
 
 // Returns pointer to an array of char*
 char** GUI_get_text(void);
