760351ebf5c5fe0b93b1a03ac4bbf76cfefcfb00
tehpola@yahoo.com <tehpola@yahoo.com@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-11-30 22:49:48 +0000

    GUI color interface (not implemented yet)

--- gui/GUI.h
+++ gui/GUI.h
@@ -5,9 +5,16 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include <ogc/gx.h>
+
 // Dimensions of array returned by get_text
 #define GUI_TEXT_WIDTH  35
 #define GUI_TEXT_HEIGHT 17
+
+// Some defined colors to make things easier
+extern GXColor GUI_color_default;
+extern GXColor GUI_color_highlighted;
+extern GXColor GUI_color_special;
 
 // Pre-formatted string (use sprintf before sending to print)
 void GUI_print(char* string);
@@ -20,6 +27,9 @@ void GUI_update(void);
 
 // Returns pointer to an array of char*
 char** GUI_get_text(void);
+
+// Returns an array of GXColor
+GXColor* GUI_get_colors(void);
 
 #endif
 
