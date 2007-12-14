c9985737902517b9caf79e99526e01cd41c100f9
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-12-14 00:37:43 +0000

    added a flag to print debug or not -DSHOW_DEBUG

--- gui/DEBUG.c
+++ gui/DEBUG.c
@@ -9,10 +9,11 @@
 char text[DEBUG_TEXT_HEIGHT][DEBUG_TEXT_WIDTH];
 
 void DEBUG_print(char* string,int pos){
-	
-	memset(text[pos],0,DEBUG_TEXT_WIDTH);
-	strncpy(text[pos], string, DEBUG_TEXT_WIDTH);
-	memset(text[DEBUG_TEXT_WIDTH-1],0,1);
+	#ifdef SHOW_DEBUG
+		memset(text[pos],0,DEBUG_TEXT_WIDTH);
+		strncpy(text[pos], string, DEBUG_TEXT_WIDTH);
+		memset(text[DEBUG_TEXT_WIDTH-1],0,1);
+	#endif
 	
 }
 
