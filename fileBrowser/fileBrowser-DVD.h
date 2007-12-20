7faef93e6617e12ddf5341e7d723f51e810dd61c
tehpola@yahoo.com <tehpola@yahoo.com@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-12-20 06:09:54 +0000

    fileBrowser-SD should now maintain full path name (required for use)

--- fileBrowser/fileBrowser-DVD.h
+++ fileBrowser/fileBrowser-DVD.h
@@ -2,6 +2,9 @@
    by emu_kidid for Mupen64-GC
  */
 
+#ifndef FILE_BROWSER_DVD_H
+#define FILE_BROWSER_DVD_H
+
 #include "fileBrowser.h"
 
 extern fileBrowser_file topLevel_DVD;
@@ -9,4 +12,6 @@ extern fileBrowser_file topLevel_DVD;
 int fileBrowser_DVD_readDir(fileBrowser_file*, fileBrowser_file**);
 int fileBrowser_DVD_readFile(fileBrowser_file*, void*, unsigned int);
 int fileBrowser_DVD_seekFile(fileBrowser_file*, unsigned int, unsigned int);
+
+#endif
 
--- fileBrowser/fileBrowser-SD.h
+++ fileBrowser/fileBrowser-SD.h
@@ -2,6 +2,9 @@
    by Mike Slegeir for Mupen64-GC
  */
 
+#ifndef FILE_BROWSER_SD_H
+#define FILE_BROWSER_SD_H
+
 #include "fileBrowser.h"
 
 extern fileBrowser_file topLevel_SD_SlotA;
@@ -11,4 +14,6 @@ int fileBrowser_SD_readDir(fileBrowser_file*, fileBrowser_file**);
 int fileBrowser_SD_readFile(fileBrowser_file*, void*, unsigned int);
 int fileBrowser_SD_writeFile(fileBrowser_file*, void*, unsigned int);
 int fileBrowser_SD_seekFile(fileBrowser_file*, unsigned int, unsigned int);
+
+#endif
 
