b83f706d44d9887ac898da49843903b45811c5a6
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-12-24 00:37:55 +0000

    fileBrowser-CARD.c and fileBrowser-CARD.h coded

---
+++ fileBrowser/fileBrowser-CARD.h
@@ -0,0 +1,19 @@
+/* fileBrowser-CARD.h - fileBrowser CARD module
+   by emu_kidid for Mupen64-GC
+ */
+
+#ifndef FILE_BROWSER_CARD_H
+#define FILE_BROWSER_CARD_H
+
+#include "fileBrowser.h"
+
+extern fileBrowser_file topLevel_CARD_SlotA;
+extern fileBrowser_file topLevel_CARD_SlotB;
+
+int fileBrowser_CARD_readDir(fileBrowser_file*, fileBrowser_file**);
+int fileBrowser_CARD_readFile(fileBrowser_file*, void*, unsigned int);
+int fileBrowser_CARD_writeFile(fileBrowser_file*, void*, unsigned int);
+int fileBrowser_CARD_seekFile(fileBrowser_file*, unsigned int, unsigned int);
+
+#endif
+
