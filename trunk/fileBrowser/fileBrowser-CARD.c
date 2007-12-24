81cde5a3ace9ec654f40651b8f6bdca5d4bf887d
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2007-12-24 00:37:55 +0000

    fileBrowser-CARD.c and fileBrowser-CARD.h coded

---
+++ trunk/fileBrowser/fileBrowser-CARD.c
@@ -0,0 +1,95 @@
+/* fileBrowser-CARD.c - fileBrowser CARD module
+   by emu_kidid for Mupen64-GC
+ */
+
+#include <ogc/card.h>
+#include "fileBrowser.h"
+
+fileBrowser_file topLevel_CARD_SlotA =
+	{ "\0", // file name
+	  CARD_SLOTA, // slot
+	  0, // offset
+	  0, // size
+	  FILE_BROWSER_ATTR_DIR
+	 };
+
+fileBrowser_file topLevel_CARD_SlotB =
+	{ "\0", // file name
+	  CARD_SLOTB, // slot
+	  0, // offset
+	  0, // size
+	  FILE_BROWSER_ATTR_DIR
+	 };
+
+int fileBrowser_CARD_readDir(fileBrowser_file* file, fileBrowser_file** dir){
+/*	DIR* sddir = NULL;
+	// Call the corresponding SDCARD function
+	int num_entries = SDCARD_readDir(&file->name, &sddir);
+	
+	// If it was not successful, just return the error
+	if(num_entries <= 0) return num_entries;
+	
+	// Convert the SDCARD data to fileBrowser_files
+	*dir = malloc( num_entries * sizeof(fileBrowser_file) );
+	int i;
+	for(i=0; i<num_entries; ++i){
+		sprintf((*dir)[i].name, "%s\\%s", file->name, sddir[i].fname);
+		(*dir)[i].offset = 0;
+		(*dir)[i].size   = sddir[i].fsize;
+		(*dir)[i].attr   = sddir[i].fattr;
+	}
+	
+	return num_entries;
+	*/
+}
+
+int fileBrowser_CARD_seekFile(fileBrowser_file* file, unsigned int where, unsigned int type){
+	if(type == FILE_BROWSER_SEEK_SET) file->offset = where;
+	else if(type == FILE_BROWSER_SEEK_CUR) file->offset += where;
+	else file->offset = file->size + where;
+	
+	return 0;
+}
+
+int fileBrowser_CARD_readFile(fileBrowser_file* file, void* buffer, unsigned int length){
+	card_file CardFile;
+	int slot = file->discoffset;
+	
+	if(CARD_Open(slot, &file->name, &CardFile) != CARD_ERROR_NOFILE){
+		if(CARD_Read(&CardFile, buffer, length, file->offset) == 0)
+			file->offset += length;
+		else 
+		{
+			CARD_Close(&CardFile);
+			return -1;
+		}
+		return length;
+   }
+	else 
+	return -1;
+}
+
+int fileBrowser_CARD_writeFile(fileBrowser_file* file, void* buffer, unsigned int length){
+	
+	card_file CardFile;
+	int slot = file->discoffset;
+	
+	//sectorsize stuff to keep memcard filesizes and writes happy
+	unsigned int SectorSize = 0;
+    CARD_GetSectorSize (slot, &SectorSize);
+	int memcardLength = length % SectorSize;
+	if(memcardLength)
+		memcardLength = (((length/SectorSize)*SectorSize) + SectorSize);
+		
+	if(CARD_Open(slot, &file->name, &CardFile) == CARD_ERROR_NOFILE){
+		if(CARD_Create(slot, &file->name, memcardLength, &CardFile)!=0)
+			return -1;
+		if(CARD_Write(&CardFile, buffer, length, 0)==0)
+			file->offset += length;
+		CARD_Close(&CardFile);
+		return length;
+	}
+	return -1;
+
+}
+
