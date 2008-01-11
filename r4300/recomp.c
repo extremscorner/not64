295445abea98d0dd6f460c49b848f04fbe747ba8
tehpola <tehpola@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2008-01-11 00:00:35 +0000

    Hopefully this fixes the 'broken' pure interpreter

--- r4300/recomp.c
+++ r4300/recomp.c
@@ -2818,7 +2818,7 @@ void prefetch_opcode(unsigned long instr)
    dst = PC;
    src = instr;
    op = instr;
-   printf("Interpreting %08x\n",op);
+   //printf("Interpreting %08x\n",op);
    recomp_ops[((src >> 26) & 0x3F)]();
    
    // tehpola: dynarec hack
