937c4f2156aa023022adbe9ceef3febf080f5bf1
tehpola@yahoo.com <tehpola@yahoo.com@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2008-02-25 00:59:08 +0000

    Adding MEM2 header to define ownership of chunks of MEM2: currently ROMCache and TexCache have 32MB and 16MB respectively with a few MB unclaimed.

---
+++ gc_memory/MEM2.h
@@ -0,0 +1,31 @@
+/* MEM2.h - MEM2 boundaries for different chunks of memory
+   by Mike Slegeir for Mupen64-Wii
+ */
+
+#ifndef MEM2_H
+#define MEM2_H
+
+// Define a MegaByte
+#define MB (1024*1024)
+
+// MEM2 begins at MEM2_LO, the Starlet's Dedicated Memory begins at MEM2_HI
+#define MEM2_LO   ((char*)0x90000000)
+#define MEM2_HI   ((char*)0x933E0000)
+#define MEM2_SIZE (MEM2_HI - MEM2_LO)
+
+// We want 32MB for our ROM Cache
+#define ROMCACHE_SIZE (32*MB)
+#define ROMCACHE_LO   (MEM2_LO)
+#define ROMCACHE_HI   (ROMCACHE_LO + ROMCACHE_SIZE)
+
+// We want 16MB for a Texture Cache
+#define TEXCACHE_SIZE (16*MB)
+#define TEXCACHE_LO   (ROMCACHE_HI)
+#define TEXCACHE_HI   (TEXCACHE_LO + TEXCACHE_SIZE)
+
+// Unclaimed MEM2
+#define UNCLAIMED_SIZE (MEM2_HI - TEXCACHE_HI)
+#define UNCLAIMED_LO   (TEXCACHE_HI)
+#define UNCLAIMED_HI   (MEM2_HI)
+
+#endif
