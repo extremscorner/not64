384a637948b432506e1c1ad7c00de4b5f781004c
tehpola <tehpola@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2008-05-23 03:36:31 +0000

    Fixed code to use hack around since WPAD interface isn't fully realized

--- trunk/gc_input/controller-Classic.c
+++ trunk/gc_input/controller-Classic.c
@@ -29,18 +29,18 @@ static int _GetKeys(int Control, BUTTONS * Keys )
 	WPAD_Read(Control, &wpad);
 	BUTTONS* c = Keys;
 	
-	int b = wpad.exp.classic.btns_held;
-	c->R_DPAD       = (b & WPAD_CLASSIC_BUTTON_RIGHT) ? 1 : 0;
-	c->L_DPAD       = (b & WPAD_CLASSIC_BUTTON_LEFT)  ? 1 : 0;
-	c->D_DPAD       = (b & WPAD_CLASSIC_BUTTON_DOWN)  ? 1 : 0;
-	c->U_DPAD       = (b & WPAD_CLASSIC_BUTTON_UP)    ? 1 : 0;
-	c->START_BUTTON = (b & WPAD_CLASSIC_BUTTON_PLUS)  ? 1 : 0;
-	c->B_BUTTON     = (b & WPAD_CLASSIC_BUTTON_B)     ? 1 : 0;
-	c->A_BUTTON     = (b & WPAD_CLASSIC_BUTTON_A)     ? 1 : 0;
+	int b = wpad.exp.classic.btns;
+	c->R_DPAD       = (b & CLASSIC_CTRL_BUTTON_RIGHT) ? 1 : 0;
+	c->L_DPAD       = (b & CLASSIC_CTRL_BUTTON_LEFT)  ? 1 : 0;
+	c->D_DPAD       = (b & CLASSIC_CTRL_BUTTON_DOWN)  ? 1 : 0;
+	c->U_DPAD       = (b & CLASSIC_CTRL_BUTTON_UP)    ? 1 : 0;
+	c->START_BUTTON = (b & CLASSIC_CTRL_BUTTON_PLUS)  ? 1 : 0;
+	c->B_BUTTON     = (b & CLASSIC_CTRL_BUTTON_B)     ? 1 : 0;
+	c->A_BUTTON     = (b & CLASSIC_CTRL_BUTTON_A)     ? 1 : 0;
 
-	c->Z_TRIG       = (b & WPAD_CLASSIC_BUTTON_ZR)     ? 1 : 0;
-	c->R_TRIG       = (b & WPAD_CLASSIC_BUTTON_FULL_R) ? 1 : 0;
-	c->L_TRIG       = (b & WPAD_CLASSIC_BUTTON_FULL_L) ? 1 : 0;
+	c->Z_TRIG       = (b & CLASSIC_CTRL_BUTTON_ZR)     ? 1 : 0;
+	c->R_TRIG       = (b & CLASSIC_CTRL_BUTTON_FULL_R) ? 1 : 0;
+	c->L_TRIG       = (b & CLASSIC_CTRL_BUTTON_FULL_L) ? 1 : 0;
 	
 	s8 substickX = getStickValue(&wpad.exp.classic.rjs, STICK_X, 7);
 	c->R_CBUTTON    = (substickX >  4)       ? 1 : 0;
@@ -53,7 +53,7 @@ static int _GetKeys(int Control, BUTTONS * Keys )
 	c->Y_AXIS       = getStickValue(&wpad.exp.classic.ljs, STICK_Y, 127);
 	
 	// X+Y quits to menu
-	return (b & WPAD_CLASSIC_BUTTON_X) && (b & WPAD_CLASSIC_BUTTON_Y);
+	return (b & CLASSIC_CTRL_BUTTON_X) && (b & CLASSIC_CTRL_BUTTON_Y);
 }
 
 static void pause(int Control){ }
