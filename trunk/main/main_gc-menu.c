7f3d1821bb22ce3fd4b03617b0f77e1105d6be4c
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2008-06-08 07:59:38 +0000

    Put old reset checking method back

--- trunk/main/main_gc-menu.c
+++ trunk/main/main_gc-menu.c
@@ -317,6 +317,8 @@ static void rsp_info_init(void){
 
 void ScanPADSandReset() {
 	PAD_ScanPads();
+	if(!((*(u32*)0xCC003000)>>16))
+		stop = 1;
 }
 void ResetCallBack() {stop = 1;}
 
@@ -376,7 +378,7 @@ static void Initialise (void){
 		isWii = 1;
 	else
 		isWii = 0;
-	SYS_SetResetCallback(ResetCallBack);	//untested
+	//SYS_SetResetCallback(ResetCallBack);	//not working, use old method for now
 }
 
 /* Reinitialize GX */ 
