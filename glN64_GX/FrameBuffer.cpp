22106fd171a359931cd760c9600eb42aee53f5d4
sepp256 <sepp256@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-10-14 02:41:34 +0000

    Fixed compiler conditional in FrameBuffer.cpp Upped GC tex cache to 2.5MB

--- glN64_GX/FrameBuffer.cpp
+++ glN64_GX/FrameBuffer.cpp
@@ -270,7 +270,7 @@ void FrameBuffer_SaveBuffer( u32 address, u16 size, u16 width, u16 height )
 	else
 		current->texture->GXrealHeight = current->texture->realHeight;*/
 
-#if def HW_RVL
+#ifdef HW_RVL
 	current->texture->textureBytes = (current->texture->GXrealWidth * current->texture->GXrealHeight) * 4;
 	current->texture->GXtexfmt = GX_TF_RGBA8;
 #else //HW_RVL
