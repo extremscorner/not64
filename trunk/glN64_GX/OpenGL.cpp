ef863d9cf5e9709a8004c8603f48252efa417d95
sepp256 <sepp256@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-10-14 02:38:57 +0000

    Fixed flipped FB textures.

--- trunk/glN64_GX/OpenGL.cpp
+++ trunk/glN64_GX/OpenGL.cpp
@@ -1030,10 +1030,17 @@ void OGL_AddTriangle( SPVertex *vertices, int v0, int v1, int v2 )
 				else
 					OGL.vertices[OGL.numVertices].s0 = (cache.current[0]->offsetS + (vertices[v[i]].s * cache.current[0]->shiftScaleS * gSP.texture.scales - gSP.textureTile[0]->fuls)) * cache.current[0]->scaleS;
 
+#ifndef __GX__
 				if (gSP.textureTile[0]->maskt)
 					OGL.vertices[OGL.numVertices].t0 = (cache.current[0]->offsetT - (vertices[v[i]].t * cache.current[0]->shiftScaleT * gSP.texture.scalet - fmod( gSP.textureTile[0]->fult, 1 << gSP.textureTile[0]->maskt ))) * cache.current[0]->scaleT;
 				else
 					OGL.vertices[OGL.numVertices].t0 = (cache.current[0]->offsetT - (vertices[v[i]].t * cache.current[0]->shiftScaleT * gSP.texture.scalet - gSP.textureTile[0]->fult)) * cache.current[0]->scaleT;
+#else //!__GX__
+				if (gSP.textureTile[0]->maskt)
+					OGL.vertices[OGL.numVertices].t0 = (cache.current[0]->offsetT + (vertices[v[i]].t * cache.current[0]->shiftScaleT * gSP.texture.scalet - fmod( gSP.textureTile[0]->fult, 1 << gSP.textureTile[0]->maskt ))) * cache.current[0]->scaleT;
+				else
+					OGL.vertices[OGL.numVertices].t0 = (cache.current[0]->offsetT + (vertices[v[i]].t * cache.current[0]->shiftScaleT * gSP.texture.scalet - gSP.textureTile[0]->fult)) * cache.current[0]->scaleT;
+#endif //__GX__
 			}
 			else
 			{
@@ -1047,7 +1054,11 @@ void OGL_AddTriangle( SPVertex *vertices, int v0, int v1, int v2 )
 			if (cache.current[0]->frameBufferTexture)
 			{
 				OGL.vertices[OGL.numVertices].s1 = (cache.current[1]->offsetS + (vertices[v[i]].s * cache.current[1]->shiftScaleS * gSP.texture.scales - gSP.textureTile[1]->fuls)) * cache.current[1]->scaleS;
+#ifndef __GX__
 				OGL.vertices[OGL.numVertices].t1 = (cache.current[1]->offsetT - (vertices[v[i]].t * cache.current[1]->shiftScaleT * gSP.texture.scalet - gSP.textureTile[1]->fult)) * cache.current[1]->scaleT;
+#else //!__GX__
+				OGL.vertices[OGL.numVertices].t1 = (cache.current[1]->offsetT + (vertices[v[i]].t * cache.current[1]->shiftScaleT * gSP.texture.scalet - gSP.textureTile[1]->fult)) * cache.current[1]->scaleT;
+#endif //__GX__
 			}
 			else
 			{
@@ -1531,10 +1542,17 @@ void OGL_DrawTexturedRect( float ulx, float uly, float lrx, float lry, float uls
 
 		if (cache.current[0]->frameBufferTexture)
 		{
+#ifndef __GX__
 			rect[0].s0 = cache.current[0]->offsetS + rect[0].s0;
 			rect[0].t0 = cache.current[0]->offsetT - rect[0].t0;
 			rect[1].s0 = cache.current[0]->offsetS + rect[1].s0;
 			rect[1].t0 = cache.current[0]->offsetT - rect[1].t0;
+#else //!__GX__
+			rect[0].s0 = cache.current[0]->offsetS + rect[0].s0;
+			rect[0].t0 = cache.current[0]->offsetT + rect[0].t0;
+			rect[1].s0 = cache.current[0]->offsetS + rect[1].s0;
+			rect[1].t0 = cache.current[0]->offsetT + rect[1].t0;
+#endif //__GX__
 		}
 
 #ifndef __GX__	
@@ -1586,10 +1604,17 @@ void OGL_DrawTexturedRect( float ulx, float uly, float lrx, float lry, float uls
 
 		if (cache.current[1]->frameBufferTexture)
 		{
+#ifndef __GX__
 			rect[0].s1 = cache.current[1]->offsetS + rect[0].s1;
 			rect[0].t1 = cache.current[1]->offsetT - rect[0].t1;
 			rect[1].s1 = cache.current[1]->offsetS + rect[1].s1;
 			rect[1].t1 = cache.current[1]->offsetT - rect[1].t1;
+#else //!__GX__
+			rect[0].s1 = cache.current[1]->offsetS + rect[0].s1;
+			rect[0].t1 = cache.current[1]->offsetT + rect[0].t1;
+			rect[1].s1 = cache.current[1]->offsetS + rect[1].s1;
+			rect[1].t1 = cache.current[1]->offsetT + rect[1].t1;
+#endif //__GX__
 		}
 
 #ifndef __GX__	
