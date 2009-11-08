bfb3016ac37d30c315271b8a7b58b93e16cb56f9
sepp256 <sepp256@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-11-08 05:24:29 +0000

    Added headers to GX_gfx source files.

--- GX_gfx/tx_GX.cpp
+++ GX_gfx/tx_GX.cpp
@@ -1,6 +1,26 @@
-/* tx_GX.cpp - N64 GX plugin, based off Hacktarux's soft_gfx
-   by sepp256 for Mupen64-GC
- */
+/**
+ * Wii64 - tx_GX.h
+ * Copyright (C) 2002 Hacktarux 
+ * Copyright (C) 2007, 2008 sepp256
+ * 
+ * N64 GX plugin, based off Hacktarux's soft_gfx
+ * by sepp256 for Mupen64-GC
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address: sepp256@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
 
 
 #include <malloc.h>
--- GX_gfx/vi_GX.h
+++ GX_gfx/vi_GX.h
@@ -1,6 +1,25 @@
-/* vi_GX.h - vi for Gamecube, based off vi_SDL
-   by Mike Slegeir for Mupen64-GC
- */
+/**
+ * Wii64 - vi_GX.h
+ * Copyright (C) 2007, 2008, 2009 Wii64 Team
+ * 
+ * vi for Gamecube by Mike Slegeir
+ * load progress bar and GX hardware scaling by sepp256
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address: sepp256@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
 
 #ifndef VI_GX_H
 #define VI_GX_H
