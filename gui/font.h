5a2242e4b70bd2cd4a1f35733cce3efd19c7bab3
sepp256 <sepp256@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-11-08 05:24:53 +0000

    Added headers to some menuV1 source files.

--- gui/font.h
+++ gui/font.h
@@ -1,6 +1,26 @@
-/*****************************************************************************
- * font.h
- *****************************************************************************/
+/**
+ * Wii64 - font.h
+ * Copyright (C) 2007, 2008, 2009 Wii64 Team
+ *
+ * IPL font functions	
+ * modified by sepp256 to work as textures instead of writing to the xfb
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address: sepp256@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
