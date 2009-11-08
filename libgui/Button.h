38893a90127df944c23e12e6a64506b588909f9b
sepp256 <sepp256@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-11-08 05:22:04 +0000

    Added headers to libGui source files.

--- libgui/Button.h
+++ libgui/Button.h
@@ -1,3 +1,23 @@
+/**
+ * Wii64 - Button.h
+ * Copyright (C) 2009 sepp256
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address: sepp256@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
+
 #ifndef BUTTON_H
 #define BUTTON_H
 
