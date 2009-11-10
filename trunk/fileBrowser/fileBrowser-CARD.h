ee0edf0841a9fa862723efc3abba0cc76ddb5e40
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-11-10 13:35:12 +0000

    fileBrowser-libfat.c now leaves EXI alone & fileBrowser Headers done

--- trunk/fileBrowser/fileBrowser-CARD.h
+++ trunk/fileBrowser/fileBrowser-CARD.h
@@ -1,6 +1,25 @@
-/* fileBrowser-CARD.h - fileBrowser CARD module
-   by emu_kidid for Mupen64-GC
- */
+/**
+ * Wii64 - fileBrowser-CARD.h
+ * Copyright (C) 2007, 2008, 2009 emu_kidid
+ * 
+ * fileBrowser module for Nintendo Gamecube Memory Cards
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address:  emukidid@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
+
 
 #ifndef FILE_BROWSER_CARD_H
 #define FILE_BROWSER_CARD_H
--- trunk/fileBrowser/fileBrowser-libfat.c
+++ trunk/fileBrowser/fileBrowser-libfat.c
@@ -1,6 +1,27 @@
-/* fileBrowser-libfat.c - fileBrowser for any devices using libfat
-   by Mike Slegeir for Mupen64-GC
- */
+/**
+ * Wii64 - fileBrowser-libfat.c
+ * Copyright (C) 2007, 2008, 2009 Mike Slegeir
+ * Copyright (C) 2007, 2008, 2009 emu_kidid
+ * 
+ * fileBrowser for any devices using libfat
+ *
+ * Wii64 homepage: http://www.emulatemii.com
+ * email address: tehpola@gmail.com
+ *                emukidid@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+**/
+
 
 #include <fat.h>
 #include <stdio.h>
@@ -117,6 +138,7 @@ static void *removalCallback (void *arg)
         }
         break;
 #endif
+/*    //Polling EXI is bad with locks, so lets not do it.
       case CARD_A:   //check which one, if removed, set as unmounted
         if(!carda->isInserted()) {
           sdNeedsUnmount=sdMounted;
@@ -129,6 +151,7 @@ static void *removalCallback (void *arg)
           sdMounted=0;
         }
         break;
+*/
     }
 #ifdef HW_RVL
     if(usbMounted) // check if the device was removed
